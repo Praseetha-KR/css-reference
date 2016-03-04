ad38eb3940016c1a6e8aefd56025b478374fb567
Praseetha-KR <voidimagineer@yahoo.com>
2016-03-04 15:58:42 +0530

    Adds error handling

--- app/index.js
+++ app/index.js
@@ -3,10 +3,10 @@
 var express     = require('express'),
     bodyParser  = require('body-parser'),
     app         = express(),
-    cssref      = require('./routes/cssref')
+    routes      = require('./routes/cssref')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api', cssref);
+app.use('/api', routes);
 
 module.exports = app;
--- app/routes/cssref.js
+++ app/routes/cssref.js
@@ -6,11 +6,10 @@ var express    = require('express'),
     sanitize   = require('../utils/sanitize');
 
 module.exports = router;
-
+/**
+ * Log details of req
+ */
 router.use(function(req, res, next) {
-    /**
-     * Log req details
-     */
     console.log(req.method + ' ' +
         req.baseUrl + req.url + ' ' +
         res.statusCode + ' ' +
@@ -19,7 +18,26 @@ router.use(function(req, res, next) {
     );
     next();
 });
+/**
+ * Router middleware to validate :name
+ */
+router.param('name', function(req, res, next, name) {
+    CssRef.findOne({ name: name }, function(err, cssref) {
+        if (err) return next(err);
+        if (!cssref) {
+            var err = new Error();
+            err.status = 404;
+            err.message = 'Reference not found'
+            return next(err);
+        }
+        req.cssref = cssref;
+        next();
+    });
+});
 
+/**
+ * API Routes
+ */
 router.get('/', function(req, res) {
     res.json({ message: 'Welcome to CSS Reference API' });
 });
@@ -30,17 +48,13 @@ router.route('/css')
         cssref.name = req.body.name;
 
         cssref.save(function(err) {
-            if (err) {
-                res.send(err);
-            }
+            if (err) res.send(err);
             res.json(sanitize(cssref));
         });
     })
     .get(function(req, res) {
         CssRef.find(function(err, cssrefs) {
-            if (err) {
-                res.send(err);
-            }
+            if (err) res.send(err);
             cssrefs = cssrefs.map(function(obj) {
                 return sanitize(obj)
             });
@@ -49,19 +63,15 @@ router.route('/css')
     });
 
 router.route('/css/:name')
-    .get(function(req, res) {
+    .get(function(req, res, next) {
         CssRef.findOne({ name: req.params.name }, function(err, cssref) {
-            if (err) {
-                res.send(err);
-            }
+            if (err) return next(err);
             res.json(sanitize(cssref));
         });
     })
     .put(function(req, res) {
         CssRef.findOne({ name: req.params.name }, function(err, cssref) {
-            if (err) {
-                res.send(err);
-            }
+            if (err) res.send(err);
             cssref.name = req.body.name;
             cssref.save(function(err) {
                 if (err) {
@@ -73,10 +83,23 @@ router.route('/css/:name')
     })
     .delete(function(req, res) {
         CssRef.remove({ name: req.params.name }, function(err, cssref) {
-            if (err) {
-                res.send(err);
-            }
+            if (err) res.send(err);
             res.sendStatus(204);
         });
     });
 
+/**
+ * Handling 404 errors
+ */
+router.get('*', function(req, res, next) {
+    var err = new Error();
+    err.status = 404;
+    err.message = 'Requested URL not found';
+    next(err);
+});
+router.use(function(err, req, res, next) {
+    if (err.status !== 404) {
+        return next();
+    }
+    res.status(err.status).send(err);
+});
